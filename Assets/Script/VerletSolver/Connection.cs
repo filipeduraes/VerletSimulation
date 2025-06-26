8d6cad637268eb20e55a9d265b43f7133bcfe1be
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2025-06-26 08:44:05 -0300

    Create solver

---
+++ Assets/Script/VerletSolver/Connection.cs
@@ -0,0 +1,13 @@
+﻿public struct Connection
+{
+    public int FirstDotIndex { get; }
+    public int SecondDotIndex { get; }
+    public float Length { get; }
+
+    public Connection(int firstDotIndex, int secondDotIndex, float length)
+    {
+        FirstDotIndex = firstDotIndex;
+        SecondDotIndex = secondDotIndex;
+        Length = length;
+    }
+}
